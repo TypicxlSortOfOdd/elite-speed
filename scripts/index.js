b0d9517c298ad412f6a3cf7d7d167766573e9145
TypicxlSortOfOdd <pjfcwjn@gmail.com>
2026-03-05 09:11:36 -0600

    Tweaks for structure and ease-of-reading. Others

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,33 @@
 const CONFIG = {
-    id: "com.TypicxlSortOfOdd.elite-speed",
-    mod_name: "Elite Speed",
-    experimental_mode: false,
+    ID: "com.TypicxlSortOfOdd.elite-speed",
+    MOD_NAME: "Elite Speed",
+    EXPERIMENTAL_MODE: false,
 };
 
 (function () {
     const api = window.SubwayBuilderAPI;
 
-    console.log(`[${CONFIG.mod_name}] Loaded! Version`);
+    console.log(`[${CONFIG.MOD_NAME}] Loaded!`);
 
     api.hooks.onGameInit(() => {
-        api.ui.showNotification(`${CONFIG.mod_name} loaded.`, `success`);
+        api.ui.showNotification(`${CONFIG.MOD_NAME} loaded.`, `success`);
     });
     try {
-        window.SubwayBuilderAPI.trains.registerTrainType({
-            id: 'shinkansen',
+        api.trains.registerTrainType({
+            id: 'Shinkansen',
             name: 'Jap. Bullet Train',
             description: 'Japanese Bullet Trains; modelled after Shinkansen.',
             compatibleTrackTypes: ['Shinkansen'],
 
+            appearance: {
+                color: '#939393'
+            },
+
             stats: {
-                maxAcceleration: 0.70,
-                maxDeceleration: 0.82,
-                maxSpeed: 85, // m/s
-                maxSpeedLocalStation: 55,
+                maxAcceleration: 0.68, // m/s | 2.44kph/s | 1.52mph/s
+                maxDeceleration: 0.9, // m/s | 3.24kph/s | 2mph/s
+                maxSpeed: 85, // m/s | 306kph | 190mph
+                maxSpeedLocalStation: 55.6, // m/s | 200kph | 124mph
                 capacityPerCar: 83,
                 carLength: 25,
                 minCars: 6,
@@ -40,28 +44,29 @@ const CONFIG = {
                 scissorsCrossoverCost: 4_500_000,
                 stopTimeSeconds: 48,
                 parallelTrackSpacing: 4.3,
-                trackClearance: 5.5,
-                maxLateralAcceleration: 0.8,
+                trackClearance: 5.5, // 0.4m Clearance
+                maxLateralAcceleration: 0.8, // m/s | 2.88kph/s | 1.78mph/s
                 minTurnRadius: 100,
                 minStationTurnRadius: 100,
                 maxSlopePercentage: 3.5,
             },
-            appearance: {
-                color: '#939393'
-            }
         });
 
-        window.SubwayBuilderAPI.trains.registerTrainType({
-            id: 'maglev',
+        api.trains.registerTrainType({
+            id: 'SCMaglev',
             name: 'Jap. Maglev',
             description: 'Japanese Maglev; modelled after the SCMaglev L0 Series.',
-            compatibleTrackTypes: ['Maglev'],
+            compatibleTrackTypes: ['SCMaglev'],
+
+            appearance: {
+                color: '#939393'
+            },
 
             stats: {
-                maxAcceleration: 1.32,
-                maxDeceleration: 1.32,
-                maxSpeed: 160, // m/s
-                maxSpeedLocalStation: 85,
+                maxAcceleration: 1.32, // m/s | 4.75kph/s | 2.95mph/s
+                maxDeceleration: 1.32, // m/s | 4.75kph/s | 2.95mph/s
+                maxSpeed: 160, // m/s | 576kph | 357mph
+                maxSpeedLocalStation: 85, // m/s | 306kph | 190mph
                 capacityPerCar: 68,
                 carLength: 24.5,
                 minCars: 4,
@@ -78,28 +83,30 @@ const CONFIG = {
                 scissorsCrossoverCost: 16_000_000,
                 stopTimeSeconds: 45,
                 parallelTrackSpacing: 4.5,
-                trackClearance: 5.5,
-                maxLateralAcceleration: 1.15,
+                trackClearance: 5.5, // 1.15m Clearance
+                maxLateralAcceleration: 1.15, // m/s | 4.14kph/s | 2.57mph/s
                 minTurnRadius: 100,
                 minStationTurnRadius: 100,
                 maxSlopePercentage: 5.5,
             },
-            appearance: {
-                color: '#939393'
-            }    
         });
     } catch (error) {
-        console.error(`[${CONFIG.mod_name}] Failed to initialize base train(s).`, error);
-        api.ui.showNotification(`${CONFIG.mod_name} ran into a problem.`);
+        console.error(`[${CONFIG.MOD_NAME}] Failed to initialize base train(s).`, error);
+        api.ui.showNotification(`${CONFIG.MOD_NAME} ran into a problem.`);
     };
 
     try {
-        if(CONFIG.experimental_mode === true) {
-            window.SubwayBuilderAPI.trains.registerTrainType({
-                id: 'vacrail',
+        if(CONFIG.EXPERIMENTAL_MODE === true) {
+            api.warn(`[${CONFIG.MOD_NAME}] Experimental mode enabled.`)
+            api.trains.registerTrainType({
+                id: 'VacRail',
                 name: 'VacRail',
                 description: 'Rail surrounded by a vacuum minimizes air resistance; modelled after vactrain concepts.',
                 compatibleTrackTypes: ['VacRail'],
+                
+                appearance: {
+                    color: '#939393'
+                },
 
                 stats: {
                     maxAcceleration: 1.45,
@@ -128,13 +135,10 @@ const CONFIG = {
                     minStationTurnRadius: 100,
                     maxSlopePercentage: 5.5,
                 },
-                appearance: {
-                    color: '#939393'
-                },
             });
         };
     } catch (error) {
-        console.error(`[${CONFIG.mod_name}] Failed to initialize experimental train(s).`, error);
-        api.ui.showNotification(`${CONFIG.mod_name} experimental ran into a problem.`);
+        console.error(`[${CONFIG.MOD_NAME}] Failed to initialize experimental train(s).`, error);
+        api.ui.showNotification(`${CONFIG.MOD_NAME} experimental ran into a problem.`);
     }
 })();
